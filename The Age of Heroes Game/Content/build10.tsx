b8faf95534706526cf4a2770008052a3d2446984
George Barber <000028882@student.thomroth.ac.uk>
2019-04-04 22:14:26 +0100

    commit

---
+++ The Age of Heroes Game/Content/build10.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="build10" tilewidth="16" tileheight="16" tilecount="621" columns="23">
+ <image source="build10.png" width="380" height="436"/>
+ <tile id="344">
+  <objectgroup draworder="index" offsetx="0" offsety="10"/>
+ </tile>
+</tileset>
---
+++ The Age of Heroes Game/Content/water1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="water1" tilewidth="16" tileheight="16" tilecount="1728" columns="48">
+ <image source="water1.png" width="768" height="576"/>
+</tileset>
