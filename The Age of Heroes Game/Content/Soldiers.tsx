08b9c2923dd794db386809876898f53f27cfbc06
George Barber <000028882@student.thomroth.ac.uk>
2019-04-10 12:42:40 +0100

    another one

---
+++ The Age of Heroes Game/Content/Soldiers.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Soldiers" tilewidth="16" tileheight="16" tilecount="384" columns="24">
+ <image source="Soldiers.png" width="384" height="256"/>
+</tileset>
